a572568561c99d2d8bea5863ef76066221490235
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-22 22:45:15 +0000

    fix(service): Réorganisation du code HTML pour une meilleure lisibilité et ajout de balises manquantes

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -17,7 +17,9 @@
 
         <div class="text-zinc-600 min-[750px]:w-[80%] space-y-4 border border-slate-200 rounded p-2">
             <div>
-                <img class="rounded" src="{{ str_starts_with($service['image'], '/images/') ? $service['image'] : '/storage/'. $service['image'] }}" alt="image d'illustration">
+                <img class="rounded"
+                    src="{{ str_starts_with($service['image'], '/images/') ? $service['image'] : '/storage/' . $service['image'] }}"
+                    alt="image d'illustration">
             </div>
             <div class="space-y-4">
                 {!! $service['description'] !!}
@@ -37,23 +39,24 @@
         <ul id="services" class="grid grid-cols-2 my-2 max-[830px]:grid-cols-1 w-full space-y-4 space-x-4">
 
             @foreach ($next_services as $service)
-                <a href="/services/{{ $service->identifier }}" data-aos="fade-up" data-aos-duration="1200"
-                    data-aos-delay="{{ $service->id * 1000 }}"
-                    class="grid grid-cols-2 gap-2 p-2 rounded bg-white shadow-sm max-h-auto">
+                <li data-aos="fade-up" data-aos-duration="1200" data-aos-delay="{{ $service->id * 1000 }}"
+                    class="grid gap-2 p-2 rounded bg-white shadow-sm max-h-auto">
 
-                    <div>
-                        <img class="object-fit max-[830px]:object-cover max-[830px]:w-full h-[136px] rounded"
-                            src="{{ $service->image }}" alt="image d'illustration de {{ $service->title }}" />
-                    </div>
+                    <a class="space-y-2" href="/services/{{ $service->identifier }}" id="contents">
+                        <div>
+                            <img class="object-fit max-[830px]:object-cover max-[830px]:w-full h-[136px] rounded"
+                                src="{{ $service->image }}" alt="image d'illustration de {{ $service->title }}" />
+                        </div>
 
-                    <div class="text-white flex flex-col justify-around">
-                        <h3 class="text-slate-400">{{ $service->title }}</h3>
-                        <p class="text-slate-600 w-full">
-                            {{ $service->summary }}
-                        </p>
-                    </div>
+                        <div class="text-white flex flex-col justify-around">
+                            <h3 class="text-slate-400">{{ $service->title }}</h3>
+                            <p class="text-slate-600 w-full">
+                                {{ $service->summary }}
+                            </p>
+                        </div>
+                    </a>
 
-                </a>
+                </li>
             @endforeach
         </ul>
 
