ea36f254d7b6259203751dceceaceb98eecc49fb
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-10-05 19:31:20 +0000

    feat(routes): Réorganisation des routes du tableau de bord, ajout des contrôleurs et mise à jour des redirections pour les services et blogs

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,13 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServiceController;
-use App\Http\Livewire\BlogCatalog;
 use App\Livewire\Auth\Login;
-use App\Livewire\BlogArticles;
 use Illuminate\Support\Facades\Route;
+
+
+use App\Livewire\DashboardServices;
+use App\Livewire\DashboardBlogArticles;
+
 
 Route::get('/', HomeController::class)->name('home');
 
@@ -31,5 +34,28 @@ Route::get('/login', Login::class)->name('login')->middleware('guest');
 
 Route::view('/a-propos', 'about')->name('about');
 
+Route::middleware(['auth'])->group(function () {
 
-require_once "dashboard.php";
+    Route::redirect('/dashboard', '/dashboard/services');
+    
+    Route::get('/dashboard/services', DashboardServices::class)->name('dashboard');
+    Route::post('/dashboard/services', [ServiceController::class, 'store'])->name('dashboard.services');
+    Route::get('/dashboard/services/create', [ServiceController::class, 'create'])->name('dashboard.services.create');
+    Route::get('/dashboard/services/{service}', [ServiceController::class, 'show'])->name('dashboard.services.show');
+    Route::put('/dashboard/services/{service}', [ServiceController::class, 'update'])->name('dashboard.services.update');
+    Route::get('/dashboard/services/{service}/edit', [ServiceController::class, 'edit'])->name('dashboard.services.edit');
+    Route::delete('/dashboard/services/{service}', [ServiceController::class, 'destroy'])->name('dashboard.services.delete');
+
+    Route::get('/dashboard/blogs', DashboardBlogArticles::class)->name('dashboard.blogs');
+
+    Route::get('/dashboard/blogs/create', [BlogController::class, 'create'])->name('dashboard.blogs.create');
+
+    Route::post('/dashboard/blogs', [BlogController::class, 'store']);
+
+    Route::get('/dashboard/blogs/{blog}', [BlogController::class, 'show'])->name('dashboard.blogs.show');
+
+    Route::delete('/dashboard/blogs/{blog}', [BlogController::class, 'destroy'])->name("dashboard.blogs.destroy");
+
+    Route::get('/dashboard/blogs/{blog}/edit', [BlogController::class, 'edit'])->name('dashboard.blogs.edit');
+    Route::put('/dashboard/blogs/{blog}/edit', [BlogController::class, 'update'])->name('dashboard.blogs.update');
+});
