f2084922f580e1124279ee60fc6a269c1819d6a3
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-10-05 19:42:51 +0000

    chore(routes): Suppression du fichier dashboard.php obsolète

--- routes/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Livewire\DashboardServices;
-use Illuminate\Support\Facades\Route;
-use App\Livewire\DashboardBlogArticles;
-use App\Http\Controllers\dashboard\BlogController;
-use App\Http\Controllers\dashboard\ServiceController;
-
